51458f4309a1d776c27d232fa3405313e70dc1a1
Mateus Brites <mateusmmo15@gmail.com>
2024-02-20 19:36:58 -0300

    add endpoint find all vagas

--- src/main/java/com/vagas/challenge/modules/vagas/controller/VagasController.java
+++ src/main/java/com/vagas/challenge/modules/vagas/controller/VagasController.java
@@ -1,7 +1,10 @@
 package com.vagas.challenge.modules.vagas.controller;
 
+import java.util.List;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,5 +37,10 @@ public class VagasController {
         var newVaga = vagasServices.create(Vaga);
 
         return ResponseEntity.status(201).body(newVaga);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<VagasEntity>> getAll() {
+        return ResponseEntity.status(200).body(vagasServices.getAll());
     }
 }
--- src/main/java/com/vagas/challenge/modules/vagas/services/VagasServices.java
+++ src/main/java/com/vagas/challenge/modules/vagas/services/VagasServices.java
@@ -1,5 +1,7 @@
 package com.vagas.challenge.modules.vagas.services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.vagas.challenge.modules.vagas.entities.VagasEntity;
@@ -17,5 +19,9 @@ public class VagasServices {
         var newVaga = vagasRepository.save(vaga);
 
         return newVaga;
+    }
+
+    public List<VagasEntity> getAll() {
+        return vagasRepository.findAll();
     }
 }
