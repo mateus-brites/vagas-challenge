239256dff488714fa341e894be887e28e8cd35d2
Mateus Brites <mateusmmo15@gmail.com>
2024-02-20 23:46:53 -0300

    add new test

--- src/test/java/com/vagas/challenge/vagas/VagasServiceTest.java
+++ src/test/java/com/vagas/challenge/vagas/VagasServiceTest.java
@@ -1,11 +1,14 @@
 package com.vagas.challenge.vagas;
 
+import java.sql.SQLException;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.ActiveProfiles;
 
 import com.vagas.challenge.modules.vagas.dto.VagasDTO;
@@ -49,6 +52,16 @@ public class VagasServiceTest {
     @Test
     @DisplayName("Shoulde not be able create a new vaga with null value")
     void createVagaFail() {
-        
+        var vaga = new VagasEntity();
+        var dto = new VagasDTO(
+            "Vagas",
+            null,
+            "Backend Java Pleno",
+            "Campinas",
+            2);
+
+        BeanUtils.copyProperties(dto, vaga);
+    
+        Assertions.assertThrows(DataIntegrityViolationException.class,() -> vagasServices.create(vaga));
     }
 }
